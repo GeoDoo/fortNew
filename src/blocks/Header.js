a30e772a4df42788eba02cb5ee6aab3e633672d6
George Karametas <georgios.karametas@ideato.info>
2018-08-10 21:56:30 +0200

    Add Menu

--- src/blocks/Header.js
+++ src/blocks/Header.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import Menu from '../elements/Menu';
+import logo from '../assets/fort.png';
 
-const Header = () => null;
+const Header = () => (
+  <div>
+    <img id="logo" src={logo} />
+    <Menu />
+  </div>
+);
 
 export default Header;
--- src/blocks/Header.test.js
+++ src/blocks/Header.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Header from './Header';
 
 describe('<Header />', () => {
   it('should render without crashing', () => {
     shallow(<Header />);
+  });
+
+  describe('should contain necessary components', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(<Header />);
+    });
+
+    it('should have a logo', () => {
+      expect(wrapper.find('#logo').length).toBe(1);
+    });
+
+    it('should have a menu', () => {
+      expect(wrapper.find('Menu').length).toBe(1);
+    });
   });
 });

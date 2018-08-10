b75b60003d30e86ab7e96d847b2391fe505338a0
George Karametas <georgios.karametas@ideato.info>
2018-08-10 21:40:44 +0200

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import logo from './assets/fort.png';
 
-const App = () => (
-  <div>
-    <img src={logo} className="App-logo" alt="logo" />
-  </div>
-);
+const App = () => <div />;
 
 export default App;

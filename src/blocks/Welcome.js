88e9bf5158eacdf78912b9b476a64cabe48430e9
George Karametas <georgios.karametas@ideato.info>
2018-08-12 06:51:11 +0200

    Add Welcome block

--- src/blocks/Welcome.js
+++ src/blocks/Welcome.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Welcome = () => <div className="welcome" />;
+const Welcome = () => (
+  <div className="welcome">
+    <h1>Welcome</h1>
+    <p>text</p>
+  </div>
+);
 
 export default Welcome;
